d656cc62d7ef3889ee4273103b2ff2d6a13a61d3
WangJiangJiaoZi <dada1253@yeah.net>
2019-06-24 23:30:34 +0800

    test:Delete reixs.spec.js

--- test/reixs.spec.js
+++
@@ -1,8 +0,0 @@
-import reixs from '../src/reixs'
-
-describe('version', function() {
-    it('__VERSION__', function(done) {
-        expect(reixs.version).toEqual('__VERSION__')
-        done()
-    })
-})
