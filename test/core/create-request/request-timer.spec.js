33473cf5ca97d259b9e8b8b72fc0636913555e29
suguoyao <suguoyao888@gmail.com>
2019-07-02 16:45:41 +0800

    test: Improve requestTimer function branch test

--- test/core/create-request/request-timer.spec.js
+++ test/core/create-request/request-timer.spec.js
@@ -9,7 +9,7 @@ function requestHandle () {
 }
 
 describe('Detect timeout', function () {
-    it('Normal request', async function (done) {
+    it('Normal request and returned false', async function (done) {
         let result
         try {
             // Set the timeout value to 5000
@@ -22,11 +22,36 @@ describe('Detect timeout', function () {
         done()
     })
 
-    it('Request timed out and returned false', async function (done) {
+    it('Request timed out and returned true', async function (done) {
         let result
         try {
             // Set the timeout value to 1000
             result = await requestTimer(requestHandle(), 1000)
+        } catch (error) {
+            result = error
+        }
+
+        expect(result.timeout).toEqual(true)
+        done()
+    })
+
+    it('Normal request when the timeout value is null', async function (done) {
+        let result
+        try {
+            result = await requestTimer(requestHandle(), null)
+        } catch (error) {
+            result = error
+        }
+
+        expect(result.timeout).toEqual(false)
+        done()
+    })
+
+    it('Request timed out and returned true when the timeout value is 0', async function (done) {
+        let result
+        try {
+            // Set the timeout value to 0
+            result = await requestTimer(requestHandle(), 0)
         } catch (error) {
             result = error
         }
